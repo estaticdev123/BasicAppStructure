6c1230068dc14a24612bb65ee653b2a66ba6e0d5
PRATIK <pm3@estatic-infotech.in>
2019-10-23 16:43:27 +0530

    commit

---
+++ app/src/main/java/com/basicappstructure/activity/ImagePreviewActivity.java
@@ -0,0 +1,15 @@
+package com.basicappstructure.activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.basicappstructure.R;
+
+public class ImagePreviewActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_preview);
+
+    }
+}
